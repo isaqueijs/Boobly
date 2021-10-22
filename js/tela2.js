4b7ebedc75e51752b0b35bbc21474c37f6c8a61c
Isaque João <isaqueijs@hotmail.com>
2021-10-21 22:47:34 -0300

    add navegacão com controle remoto, menu principal

--- js/tela2.js
+++ js/tela2.js
@@ -14,26 +14,42 @@
  *      limitations under the License.
  */
 
-
 window.onload = function() {
+
+	document.getElementById("play").focus();
 
 	// add eventListener for keydown
 	document.addEventListener('keydown', function(e) {
 		switch (e.keyCode) {
-		case 37:
-			alert("TETANDO IR PRA ONDE??") // LEFT arrow
+		case 37: // LEFT arrow
 			break;
 		case 38:
-			alert("TU NÃO VAI PRA CIMA!") // UP arrow
+			if (document.activeElement === document.getElementById("exit")) {
+				document.getElementById("ranking").focus()
+			} else if (document.activeElement === document
+					.getElementById("ranking")) {
+				document.getElementById("play").focus()
+			} else if (document.activeElement === document
+					.getElementById("play")) {
+				document.getElementById("exit").focus()
+			} else {
+				document.getElementById("play").focus()
+			} // UP arrow
 			break;
-		case 39:
-			alert("DIREITA????") // RIGHT arrow
+		case 39: // RIGHT arrow
 			break;
 		case 40:
-			alert("VAI PRA BAIXO???") // DOWN arrow
+			if (document.activeElement === document.getElementById("play")) {
+				document.getElementById("ranking").focus()
+			} else if (document.activeElement === document
+					.getElementById("ranking")) {
+				document.getElementById("exit").focus()
+			} else {
+				document.getElementById("play").focus()
+			}// DOWN arrow
 			break;
 		case 13:
-			alert("TEM CERTEZA???") // OK button
+			document.activeElement.click() // OK button
 			break;
 		case 10009: // RETURN button
 			tizen.application.getCurrentApplication().exit();
