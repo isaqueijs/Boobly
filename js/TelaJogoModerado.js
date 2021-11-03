5ea33f1864ac4fc5a35022f47f6be1104b48e22d
Isaque João <isaqueijs@hotmail.com>
2021-11-02 23:27:07 -0300

    jobabilidade completa - Tela Nível Médio

--- js/TelaJogoModerado.js
+++ js/TelaJogoModerado.js
@@ -26,16 +26,114 @@ function startTimer(duration, display){
 	}, 1000);
 }
 
+var count = 0;
+var par = [];
+var id = 1;
+
+var array1 = [1, 2, 3, 4];
+var array2 = [5, 6, 7, 8];
+var array3 = [9, 10, 11, 12];
 
 window.onload = function(){
-	var duration = 60 * 0.10; //conversao para segundos
+	var duration = 60 * 10; //conversao para segundos
 	var display = document.querySelector("#timer"); //Elemento para exibir o timer
 	
 	startTimer(duration, display); //inicia a função
 	
-//	var array1 = [1, 2, 3, 4];
-//	var array2 = [5, 6, 7, 8];
-//	var array3 = [9, 10, 11, 12];
+	for (var int = 1; int <= 12; int++) {
+		document.getElementById(int).addEventListener('click', flipCard);
+	}
+
+	
+	var obj = document.getElementById("1");
+	obj.focus();
+	
+	// add eventListener for keydown
+	document.addEventListener('keydown', function(e) {
+		switch (e.keyCode) {
+		case 37: // LEFT arrow
+			 if (array1.includes(id)) {
+				id = (array1.indexOf(id) === 0) ? array1[array1.length - 1] : id - 1;
+				document.getElementById(id).focus();
+			}
+
+			else if (array2.includes(id)) {
+				id = (array2.indexOf(id) === 0) ? array2[array2.length - 1] : id - 1;
+				document.getElementById(id).focus();
+			} else if (array3.includes(id)) {
+				id = (array3.indexOf(id) === 0) ? array3[array2.length - 1] : id - 1;
+				document.getElementById(id).focus();
+			}
+			break;
+		case 38:
+			if (array1.includes(id)) {
+				id = array3[array1.indexOf(id)];
+				document.getElementById(id).focus();
+			} else if (array2.includes(id)) {
+				id = array1[array2.indexOf(id)];
+				document.getElementById(id).focus();
+			} else if (array3.includes(id)) {
+				id = array2[array3.indexOf(id)];
+				document.getElementById(id).focus();
+			}
+			break;
+		case 39: // RIGHT arrow
+			if (array1.includes(id)) {
+				id = (array1.indexOf(id) === array1.length - 1) ? array1[0] : id + 1;
+				document.getElementById(id).focus();
+			}
+
+			else if (array2.includes(id)) {
+				id = (array2.indexOf(id) === array2.length - 1) ? array2[0] : id + 1;
+				document.getElementById(id).focus();
+			} else if (array3.includes(id)) {
+				id = (array3.indexOf(id) === array2.length - 1) ? array3[0] : id + 1;
+				document.getElementById(id).focus();
+			}
+			break;
+		case 40:
+			if (array3.includes(id)) {
+				id = array1[array3.indexOf(id)];
+				document.getElementById(id).focus();
+			} else if (array2.includes(id)) {
+				id = array3[array2.indexOf(id)];
+				document.getElementById(id).focus();
+			} else if (array1.includes(id)) {
+				id = array2[array1.indexOf(id)];
+				document.getElementById(id).focus();
+			}
+			// DOWN arrow
+			break;
+		case 13:
+			if (isFaceUp(id)) {				
+				// OK button
+				break;
+			} else {
+				document.activeElement.click();
+			}
+
+			if (par.length === 2) {
+				par = [];
+			}
+
+			if (par[length] !== id) {
+				par.push(id);
+
+				if (par.length === 2) {
+					acertouPar();
+					venceu();
+				}
+			}
+
+			break;
+		case 10009: // RETURN button
+
+			break;
+		default:
+			console.log('Key code : ' + e.keyCode);
+		break;
+		}
+	});
 }
 
 
@@ -168,6 +266,7 @@ function createIdWithTech(tech) {
 	return tech + parseInt( Math.random() * 1000); //Math... numero randomico
 }
 
+*/
 
 
 function flipCard() {
@@ -176,6 +275,32 @@ function flipCard() {
 }
 
 
+function isFaceUp(id) {
+	return document.getElementById(id).classList.contains("flip");
+}
 
-*/
+function acertouPar() {
+	
+	var obj1 = document.getElementById(par[0]).childNodes.item(1).childNodes.item(1).attributes.item(1).nodeValue;
+	var obj2 = document.getElementById(par[1]).childNodes.item(1).childNodes.item(1).attributes.item(1).nodeValue;
+	
+	
+	if (obj1 !== obj2) {
+		
+		setTimeout(function() {
+			document.getElementById(par[0]).classList.remove("flip");
+			document.getElementById(par[1]).classList.remove("flip");
+		}, 1000);	
+		
+	} else {
+		console.log("Acertou o par!");
+	}
+}
 
+function venceu() {
+	
+	count = document.querySelectorAll(".flip");
+	if (count.length === 12) {
+		window.location.href = "TelaVenceu.html";
+	}
+}
